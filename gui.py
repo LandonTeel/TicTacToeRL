c79b196c94c0c15ea9c337fd6b8dd432327299eb
Evan Ariail <evanariail26@gmail.com>
2024-08-23 21:23:09 -0400

    tictactoe lines

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import pygame
+import numpy as np
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -6,12 +7,31 @@ WHITE = (255, 255, 255)
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+clock = pygame.time.Clock()
 
 pygame.display.set_caption("TicTacToeERL")
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+scale = 67
+
+points = []
+points.append(np.array([-1, 3]))
+points.append(np.array([1, 3]))
+points.append(np.array([3, 1]))
+points.append(np.array([3, -1]))
+points.append(np.array([1, -3]))
+points.append(np.array([-1, -3]))
+points.append(np.array([-3, -1]))
+points.append(np.array([-3, 1]))
+
+def to_screen_coords(point):
+    return(int(SCREEN_WIDTH/2 + scale*point[0]), int(SCREEN_HEIGHT/2 + scale*point[1]))
+
 def main():
     while True:
+
+        clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -23,6 +43,10 @@ def main():
         
         screen.fill(WHITE)
 
-        pygame.display.update()
+        pygame.draw.line(screen, BLACK, to_screen_coords(points[0]), to_screen_coords(points[5]), 5)
+        pygame.draw.line(screen, BLACK, to_screen_coords(points[1]), to_screen_coords(points[4]), 5)   
+        pygame.draw.line(screen, BLACK, to_screen_coords(points[2]), to_screen_coords(points[7]), 5)   
+        pygame.draw.line(screen, BLACK, to_screen_coords(points[3]), to_screen_coords(points[6]), 5)
+        pygame.display.flip()
 
 main()
