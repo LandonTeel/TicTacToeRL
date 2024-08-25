07da5c6faf61ede5fb06ad66003c173ecd4d4913
lukesj07 <lukesjennings07@gmail.com>
2024-08-25 19:02:43 -0400

    replace gui.py

--- gui.py
+++ gui.py
@@ -3,269 +3,139 @@ import numpy as np
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+LINE_WIDTH = 5
 
-clock = pygame.time.Clock()
 pygame.init()
+clock = pygame.time.Clock()
 font = pygame.font.Font('Cascadia.ttf', 12)
-
-pygame.display.set_caption("TicTacToeRL")
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("TicTacToeRL")
 
-scale = 67
-scale2 = 140
+def to_screen_coords(point, scale):
+    return (int(SCREEN_WIDTH / 2 + scale * point[0]), int(SCREEN_HEIGHT / 2 + scale * point[1]))
 
-points = []
-points.append(np.array([-1, 3]))
-points.append(np.array([1, 3]))
-points.append(np.array([3, 1]))
-points.append(np.array([3, -1]))
-points.append(np.array([1, -3]))
-points.append(np.array([-1, -3]))
-points.append(np.array([-3, -1]))
-points.append(np.array([-3, 1]))
+def draw_grid(screen, scale):
+    pygame.draw.line(screen, BLACK, to_screen_coords([-3, 1], scale), to_screen_coords([3, 1], scale), LINE_WIDTH)
+    pygame.draw.line(screen, BLACK, to_screen_coords([-3, -1], scale), to_screen_coords([3, -1], scale), LINE_WIDTH)
+    pygame.draw.line(screen, BLACK, to_screen_coords([-1, 3], scale), to_screen_coords([-1, -3], scale), LINE_WIDTH)
+    pygame.draw.line(screen, BLACK, to_screen_coords([1, 3], scale), to_screen_coords([1, -3], scale), LINE_WIDTH)
 
-boxes = []
-boxes.append(np.array([-3,3]))
-boxes.append(np.array([0,3]))
-boxes.append(np.array([3,3]))
-boxes.append(np.array([-3,0]))
-boxes.append(np.array([0,0]))
-boxes.append(np.array([3,0]))
-boxes.append(np.array([-3,-3]))
-boxes.append(np.array([0,-3]))
-boxes.append(np.array([3,-3]))
+def draw_shapes(screen, circles, xs):
+    for circle_coords in circles:
+        pygame.draw.circle(screen, BLACK, circle_coords, 50, LINE_WIDTH)
+    for x_coords in xs:
+        pygame.draw.line(screen, BLACK, (x_coords[0] - 50, x_coords[1] - 50), (x_coords[0] + 50, x_coords[1] + 50), LINE_WIDTH)
+        pygame.draw.line(screen, BLACK, (x_coords[0] + 50, x_coords[1] - 50), (x_coords[0] - 50, x_coords[1] + 50), LINE_WIDTH)
 
-circles_to_draw = []
-xs_to_draw = []
+def draw_winning_line(screen, start, end):
+    pygame.draw.line(screen, BLACK, start, end, LINE_WIDTH)
 
-cleartext = font.render('Clear', True, BLACK, WHITE)
-cleartext_rect = cleartext.get_rect()
-cleartext_rect.bottomleft = (20, 580)
-clear_rect = pygame.Rect(cleartext_rect.left - 5, cleartext_rect.top - 5, cleartext_rect.width + 10, cleartext_rect.height + 10)
+class TicTacToeGame:
+    def __init__(self):
+        self.clear_button = self.setup_clear_button()
+        self.boxes = self.setup_boxes()
+        self.square_array = [[2 for _ in range(3)] for _ in range(3)]
+        self.circles_to_draw = []
+        self.xs_to_draw = []
+        self.state = 1  # 0 = O; 1 = X
+        self.winner = None
+        self.winning_line = None
 
-#0 = O, 1 = X, 2 = Clear, 3 = FinishedGame
-square_array = [
-    [2, 2, 2],
-    [2, 2, 2],
-    [2, 2, 2],
-]
+    def setup_clear_button(self):
+        cleartext = font.render('Clear', True, BLACK, WHITE)
+        cleartext_rect = cleartext.get_rect()
+        cleartext_rect.bottomleft = (20, 580)
+        return cleartext, cleartext_rect
 
+    def setup_boxes(self):
+        boxes = []
+        offset = [-175, -50, 75]
+        for y in offset:
+            row = []
+            for x in offset:
+                rect = pygame.Rect(SCREEN_WIDTH / 2 + x, SCREEN_HEIGHT / 2 + y, 100, 100)
+                row.append(rect)
+            boxes.append(row)
+        return boxes
 
-def to_screen_coords(point):
-    return(int(SCREEN_WIDTH/2 + scale*point[0]), int(SCREEN_HEIGHT/2 + scale*point[1]))
+    def clear_board(self):
+        self.square_array = [[2 for _ in range(3)] for _ in range(3)]
+        self.circles_to_draw.clear()
+        self.xs_to_draw.clear()
+        self.state = 1
+        self.winner = None
+        self.winning_line = None
+        self.render()
 
-def boxes_to_screen_coords(point):
-    return(int(SCREEN_WIDTH/2 + scale2*point[0]), int(SCREEN_HEIGHT/2 + scale2*point[1]))
+    def check_win(self):
+        lines = [
+            [(0, 0), (0, 1), (0, 2)],  # top row
+            [(1, 0), (1, 1), (1, 2)],  # middle row
+            [(2, 0), (2, 1), (2, 2)],  # bottom row
+            [(0, 0), (1, 0), (2, 0)],  # left col
+            [(0, 1), (1, 1), (2, 1)],  # middle col
+            [(0, 2), (1, 2), (2, 2)],  # right col
+            [(0, 0), (1, 1), (2, 2)],  # diag tl br
+            [(0, 2), (1, 1), (2, 0)],  # diag tr bl
+        ]
 
-def draw_circle(box):
-    screen_coords = boxes_to_screen_coords(box)
-    circles_to_draw.append(screen_coords)
+        for line in lines:
+            p1, p2, p3 = line
+            if self.square_array[p1[0]][p1[1]] == self.square_array[p2[0]][p2[1]] == self.square_array[p3[0]][p3[1]] != 2:
+                self.winner = self.square_array[p1[0]][p1[1]]
+                self.winning_line = (self.boxes[p1[0]][p1[1]].center, self.boxes[p3[0]][p3[1]].center)
+                return True
+        return False
 
-def draw_x(box):
-    screen_coords = boxes_to_screen_coords(box)
-    xs_to_draw.append(screen_coords)
+    def render(self):
+        screen.fill(WHITE)
+        
+        draw_grid(screen, 67)
+        draw_shapes(screen, self.circles_to_draw, self.xs_to_draw)
+        pygame.draw.rect(screen, BLACK, self.clear_button[1], 3)
+        screen.blit(*self.clear_button)
+        
+        if self.winning_line:
+            draw_winning_line(screen, *self.winning_line)
+        
+        pygame.display.flip()
 
-def clear():
-    global square_array
-    global box0_rect, box1_rect, box2_rect, box3_rect, box4_rect, box5_rect, box6_rect, box7_rect, box8_rect
-    square_array = [
-    [2, 2, 2],
-    [2, 2, 2],
-    [2, 2, 2],
-    ]
+    def handle_click(self, mouse_pos):
+        if self.clear_button[1].collidepoint(mouse_pos):
+            self.clear_board()
+            return
+        
+        if self.winner:
+            return  # stop moves from happening when game won
 
-    circles_to_draw.clear()
-    xs_to_draw.clear()
-
-    screen.fill(WHITE)
-
-    pygame.draw.line(screen, BLACK, to_screen_coords(points[0]), to_screen_coords(points[5]), 5)
-    pygame.draw.line(screen, BLACK, to_screen_coords(points[1]), to_screen_coords(points[4]), 5)   
-    pygame.draw.line(screen, BLACK, to_screen_coords(points[2]), to_screen_coords(points[7]), 5)   
-    pygame.draw.line(screen, BLACK, to_screen_coords(points[3]), to_screen_coords(points[6]), 5)
-
-    pygame.draw.rect(screen, BLACK, clear_rect, 3)
-    screen.blit(cleartext, cleartext_rect)
-
-    box0_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 - 175), (SCREEN_HEIGHT/2 - 175), 100, 100))
-    box1_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 - 50), (SCREEN_HEIGHT/2 - 175), 100, 100))
-    box2_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 + 75), (SCREEN_HEIGHT/2 - 175), 100, 100))
-    box3_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 - 175), (SCREEN_HEIGHT/2 - 50), 100, 100))
-    box4_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 - 50), (SCREEN_HEIGHT/2 - 50), 100, 100))
-    box5_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 + 75), (SCREEN_HEIGHT/2 - 50), 100, 100))
-    box6_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 - 175), (SCREEN_HEIGHT/2 + 75), 100, 100))
-    box7_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 - 50), (SCREEN_HEIGHT/2 + 75), 100, 100))
-    box8_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 + 75), (SCREEN_HEIGHT/2 + 75), 100, 100))
-
-    
-    pygame.display.update()
-
-def win():
-    global box0_rect, box1_rect, box2_rect, box3_rect, box4_rect, box5_rect, box6_rect, box7_rect, box8_rect
-    global square_array
-    if square_array[0] == [1, 1, 1]:
-        pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH/2 - 200), (SCREEN_HEIGHT/2 - 140)), ((SCREEN_WIDTH/2 + 200), (SCREEN_HEIGHT/2 - 140)) ,5)
-    elif square_array[0] == [0, 0, 0]:
-        pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH/2 - 200), (SCREEN_HEIGHT/2 - 140)), ((SCREEN_WIDTH/2 + 200), (SCREEN_HEIGHT/2 - 140)) ,5)
-    elif square_array[1] == [1, 1, 1]:
-        pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH/2 - 200), (SCREEN_HEIGHT/2 - 0)), ((SCREEN_WIDTH/2 + 200), (SCREEN_HEIGHT/2 - 0)) ,5)
-    elif square_array[1] == [0, 0, 0]:
-        pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH/2 - 200), (SCREEN_HEIGHT/2 - 0)), ((SCREEN_WIDTH/2 + 200), (SCREEN_HEIGHT/2 - 0)) ,5)
-    elif square_array[2] == [1, 1, 1]:
-        pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH/2 - 200), (SCREEN_HEIGHT/2 + 140)), ((SCREEN_WIDTH/2 + 200), (SCREEN_HEIGHT/2 + 140)) ,5)
-    elif square_array[2] == [0, 0, 0]:
-        pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH/2 - 200), (SCREEN_HEIGHT/2 + 140)), ((SCREEN_WIDTH/2 + 200), (SCREEN_HEIGHT/2 + 140)) ,5)
-    elif [square_array[i][0] for i in range(3)] == [1, 1, 1]:
-        pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH/2 - 140), (SCREEN_HEIGHT/2 - 200)), ((SCREEN_WIDTH/2 - 140), (SCREEN_HEIGHT/2 + 200)) ,5)
-    elif [square_array[i][0] for i in range(3)] == [0, 0, 0]:
-        pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH/2 - 140), (SCREEN_HEIGHT/2 - 200)), ((SCREEN_WIDTH/2 - 140), (SCREEN_HEIGHT/2 + 200)) ,5)
-    elif [square_array[i][1] for i in range(3)] == [1, 1, 1]:
-        pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH/2 - 0), (SCREEN_HEIGHT/2 - 200)), ((SCREEN_WIDTH/2 - 0), (SCREEN_HEIGHT/2 + 200)) ,5)
-    elif [square_array[i][1] for i in range(3)] == [0, 0, 0]:
-        pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH/2 - 0), (SCREEN_HEIGHT/2 - 200)), ((SCREEN_WIDTH/2 - 0), (SCREEN_HEIGHT/2 + 200)) ,5)
-    elif [square_array[i][2] for i in range(3)] == [1, 1, 1]:
-        pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH/2 + 140), (SCREEN_HEIGHT/2 + 200)), ((SCREEN_WIDTH/2 + 140), (SCREEN_HEIGHT/2 - 200)) ,5)
-    elif [square_array[i][2] for i in range(3)] == [0, 0, 0]:
-        pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH/2 + 140), (SCREEN_HEIGHT/2 + 200)), ((SCREEN_WIDTH/2 + 140), (SCREEN_HEIGHT/2 - 200)) ,5)
-    elif square_array[0][0] == 1 and square_array[1][1] == 1 and square_array[2][2] == 1:
-        pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH/2 -200), (SCREEN_HEIGHT/2 - 200)), ((SCREEN_WIDTH/2 + 200), (SCREEN_HEIGHT/2 + 200)) ,5)
-    elif square_array[0][0] == 0 and square_array[1][1] == 0 and square_array[2][2] == 0:
-        pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH/2 -200), (SCREEN_HEIGHT/2 - 200)), ((SCREEN_WIDTH/2 + 200), (SCREEN_HEIGHT/2 + 200)) ,5)
-    elif square_array[0][2] == 1 and square_array[1][1] == 1 and square_array[2][0] == 1:
-        pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH/2 +200), (SCREEN_HEIGHT/2 - 200)), ((SCREEN_WIDTH/2 - 200), (SCREEN_HEIGHT/2 + 200)) ,5)
-    elif square_array[0][2] == 0 and square_array[1][1] == 0 and square_array[2][0] == 0:
-        pygame.draw.line(screen, BLACK, ((SCREEN_WIDTH/2 +200), (SCREEN_HEIGHT/2 - 200)), ((SCREEN_WIDTH/2 - 200), (SCREEN_HEIGHT/2 + 200)) ,5)
-    pygame.display.update()
+        for i, row in enumerate(self.boxes):
+            for j, box in enumerate(row):
+                if box.collidepoint(mouse_pos) and self.square_array[i][j] == 2:
+                    if self.state == 0:
+                        self.circles_to_draw.append(box.center)
+                        self.square_array[i][j] = 0
+                    else:
+                        self.xs_to_draw.append(box.center)
+                        self.square_array[i][j] = 1
+                    self.state = 1 - self.state
+                    if self.check_win():
+                        self.render()
+                    return
 
 def main():
-    state = 1
-    global square_array
-
+    game = TicTacToeGame()
+    game.clear_board()
     while True:
-
         clock.tick(60)
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                if state == 0 and square_array[0][0] == 2 and box0_rect.collidepoint(mouse_pos):
-                    draw_circle(np.array([-1,-1]))
-                    state = 1
-                    square_array[0][0] = 0
-                elif state == 0 and square_array[0][1] == 2 and box1_rect.collidepoint(mouse_pos):
-                    draw_circle(np.array([0,-1]))
-                    state = 1
-                    square_array[0][1] = 0
-                elif state == 0 and square_array[0][2] == 2 and box2_rect.collidepoint(mouse_pos):
-                    draw_circle(np.array([1,-1]))
-                    state = 1
-                    square_array[0][2] = 0
-                elif state == 0 and  square_array[1][0] == 2 and box3_rect.collidepoint(mouse_pos):
-                    draw_circle(np.array([-1,0]))
-                    state = 1
-                    square_array[1][0] = 0
-                elif state == 0 and  square_array[1][1] == 2 and box4_rect.collidepoint(mouse_pos):
-                    draw_circle(np.array([0,0]))
-                    state = 1
-                    square_array[1][1] = 0
-                elif state == 0 and  square_array[1][2] == 2 and box5_rect.collidepoint(mouse_pos):
-                    draw_circle(np.array([1,0]))
-                    state = 1
-                    square_array[1][2] = 0
-                elif state == 0 and  square_array[2][0] == 2 and box6_rect.collidepoint(mouse_pos):
-                    draw_circle(np.array([-1,1]))
-                    state = 1
-                    square_array[2][0] = 0
-                elif state == 0 and  square_array[2][1] == 2 and box7_rect.collidepoint(mouse_pos):
-                    draw_circle(np.array([0,1]))
-                    state = 1
-                    square_array[2][1] = 0
-                elif state == 0 and  square_array[2][2] == 2 and box8_rect.collidepoint(mouse_pos):
-                    draw_circle(np.array([1,1]))
-                    state = 1
-                    square_array[2][2] = 0
-                elif state == 1 and  square_array[0][0] == 2 and box0_rect.collidepoint(mouse_pos):
-                    draw_x(np.array([-1,-1]))
-                    state = 0
-                    square_array[0][0] = 1
-                elif state == 1 and  square_array[0][1] == 2 and box1_rect.collidepoint(mouse_pos):
-                    draw_x(np.array([0,-1]))
-                    state = 0
-                    square_array[0][1] = 1
-                elif state == 1 and  square_array[0][2] == 2 and box2_rect.collidepoint(mouse_pos):
-                    draw_x(np.array([1,-1]))
-                    state = 0
-                    square_array[0][2] = 1
-                elif state == 1 and  square_array[1][0] == 2 and box3_rect.collidepoint(mouse_pos):
-                    draw_x(np.array([-1,0]))
-                    state = 0
-                    square_array[1][0] = 1
-                elif state == 1 and  square_array[1][1] == 2 and box4_rect.collidepoint(mouse_pos):
-                    draw_x(np.array([0,0]))
-                    state = 0
-                    square_array[1][1] = 1
-                elif state == 1 and  square_array[1][2] == 2 and box5_rect.collidepoint(mouse_pos):
-                    draw_x(np.array([1,0]))
-                    state = 0
-                    square_array[1][2] = 1
-                elif state == 1 and  square_array[2][0] == 2 and box6_rect.collidepoint(mouse_pos):
-                    draw_x(np.array([-1,1]))
-                    state = 0
-                    square_array[2][0] = 1
-                elif state == 1 and  square_array[2][1] == 2 and box7_rect.collidepoint(mouse_pos):
-                    draw_x(np.array([0,1]))
-                    state = 0
-                    square_array[2][1] = 1
-                elif state == 1 and  square_array[2][2] == 2 and box8_rect.collidepoint(mouse_pos):
-                    draw_x(np.array([1,1]))
-                    state = 0
-                    square_array[2][2] = 1
-
-                elif clear_rect.collidepoint(mouse_pos):
-                    clear()
-                
-
-            
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    exit()       
-        
-        screen.fill(WHITE)
-
-        pygame.draw.line(screen, BLACK, to_screen_coords(points[0]), to_screen_coords(points[5]), 5)
-        pygame.draw.line(screen, BLACK, to_screen_coords(points[1]), to_screen_coords(points[4]), 5)   
-        pygame.draw.line(screen, BLACK, to_screen_coords(points[2]), to_screen_coords(points[7]), 5)   
-        pygame.draw.line(screen, BLACK, to_screen_coords(points[3]), to_screen_coords(points[6]), 5)
-
-        pygame.draw.rect(screen, BLACK, clear_rect, 3)
-        screen.blit(cleartext, cleartext_rect)
-        
-        box0_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 - 175), (SCREEN_HEIGHT/2 - 175), 100, 100))
-        box1_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 - 50), (SCREEN_HEIGHT/2 - 175), 100, 100))
-        box2_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 + 75), (SCREEN_HEIGHT/2 - 175), 100, 100))
-        box3_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 - 175), (SCREEN_HEIGHT/2 - 50), 100, 100))
-        box4_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 - 50), (SCREEN_HEIGHT/2 - 50), 100, 100))
-        box5_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 + 75), (SCREEN_HEIGHT/2 - 50), 100, 100))
-        box6_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 - 175), (SCREEN_HEIGHT/2 + 75), 100, 100))
-        box7_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 - 50), (SCREEN_HEIGHT/2 + 75), 100, 100))
-        box8_rect = pygame.draw.rect(screen, WHITE, pygame.Rect((SCREEN_WIDTH/2 + 75), (SCREEN_HEIGHT/2 + 75), 100, 100))
-
-        for circle_coords in circles_to_draw:
-            pygame.draw.circle(screen, BLACK, circle_coords, 50, 5)
-        
-        for x_coords in xs_to_draw:
-            pygame.draw.line(screen, BLACK, ((x_coords[0] - 50, x_coords[1] - 50)), ((x_coords[0] + 50, x_coords[1] + 50)), 5)
-            pygame.draw.line(screen, BLACK, ((x_coords[0] + 50, x_coords[1] - 50)), ((x_coords[0] - 50, x_coords[1] + 50)), 5)
-        win()
-        pygame.display.flip()
-
+                game.handle_click(event.pos)
+        game.render()
 
 if __name__ == "__main__":
     main()
