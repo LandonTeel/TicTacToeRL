a61f891410037478cca223d05ee2c5471ee16b41
CalebBunch <rcbcg123@gmail.com>
2024-09-17 12:01:21 -0400

    training loop

--- supervised_model.py
+++ supervised_model.py
@@ -4,6 +4,9 @@ from torch.utils.data import DataLoader
 from dataset import return_data
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+lr = 1e-3
+batch_size = 32
+epochs = 10
 
 class Model(nn.Module):
     def __init__(self) -> None:
@@ -20,17 +23,37 @@ class Model(nn.Module):
         out = self.layers(x)
         return out
 
-def train():
+def train() -> None:
     model = Model().to(device)
+
     dataset = return_data()
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
-    for inputs, labels in dataloader:
-        # TODO: train model
-        pass
-    
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    loss_function = nn.MSELoss()
+    optim = torch.optim.AdamW(model.parameters(), lr=lr)
+
+    i = 0
+    for epoch in range(epochs):
+        print(f"\nepoch: {epoch}\n")
+        for batch_inputs, batch_labels in dataloader:
+            for idx in range(len(batch_inputs)):
+                inp, label = batch_inputs[idx].to(device).float(), batch_labels[idx].to(device).float()
+
+                optim.zero_grad()
+                prediction = model(inp)
+                loss = loss_function(prediction, label)
+
+                loss.backward()
+                optim.step()
+
+                if i % 100 == 0:
+                    print(f"loss: {loss.item()}")
+
+                i += 1
+
 
 def main():
-    pass
+    train()
 
 if __name__ == "__main__":
     main()
